c9ebb595941edacbf7abe6384a27db14ac5ed507
MGAronya <20jzhang@stu.edu.cn>
2022-07-22 22:32:44 +0800

    《Go语言学习笔记》5.2-5.3

---
+++ 5.2/test.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+func main() {
+	{
+		var a [4]int
+		b := [4]int{2, 5}
+		c := [4]int{5, 3: 10} //可指定索引初始化
+		d := [...]int{1, 2, 3}
+		e := [...]int{10, 3: 100}
+		fmt.Println(a, b, c, d, e)
+	}
+	{ //对于结构等复合类型，可省略元素初始化类型标签
+		type user struct {
+			name string
+			age  byte
+		}
+		d := [...]user{
+			{"Tom", 20},
+			{"Mary", 18},
+		}
+		fmt.Printf("%#v\n", d)
+	}
+	{ //定义多维数组时，仅第一维允许使用...
+		a := [2][2]int{
+			{1, 2},
+			{3, 4},
+		}
+		b := [...][2]int{
+			{10, 20},
+			{30, 40},
+		}
+		c := [...][2][2]int{
+			{
+				{1, 2},
+				{3, 4},
+			},
+			{
+				{10, 20},
+				{30, 40},
+			},
+		}
+		fmt.Println(a)
+		fmt.Println(b)
+		fmt.Println(c)
+	}
+	{ //内置函数len和cap都返回第一维度长度
+		a := [2]int{}
+		b := [...][2]int{
+			{10, 20},
+			{30, 40},
+			{50, 60},
+		}
+		println(len(a), cap(a))
+		println(len(b), cap(b))
+		println(len(b[0]), cap(b[0]))
+	}
+	{
+		var a, b [2]int
+		println(a == b)
+		c := [2]int{1, 2}
+		d := [2]int{0, 1}
+		println(c != d) //支持==，!=操作，比较的是类型，即存储的元素类型和长度是否一致
+	}
+	{
+		x, y := 10, 20
+		a := [...]*int{&x, &y}
+		p := &a
+		fmt.Printf("%T, %v\n", a, a)
+		fmt.Printf("%T, %v\n", p, p)
+	}
+	{
+		a := [...]int{1, 2}
+		println(&a, &a[0], &a[1])
+	}
+	{
+		a := [111]int{1, 2}
+		p := &a
+		p[1] += 10
+
+	}
+}
